ff8b3be6fe5281390ab1ff6a7de406b4976c7f4a
Vincent Feng <iVincentFeng@live.com>
2016-05-22 13:29:23 +0800

    Template method

---
+++ TemplateMethod/BeverageMaker.cs
@@ -0,0 +1,25 @@
+﻿namespace TemplateMethod
+{
+    public abstract class BeverageMaker
+    {
+        public void PrepareBeverage()
+        {
+            this.BoilWater();
+            this.Brew();
+            this.PourBeverageIntoCup();
+            this.AddCondiments();
+        }
+
+        protected abstract void Brew();
+
+        protected abstract void AddCondiments();
+
+        private void BoilWater()
+        {
+        }
+
+        private void PourBeverageIntoCup()
+        {
+        }
+    }
+}
---
+++ TemplateMethod/CoffeeMaker.cs
@@ -0,0 +1,13 @@
+﻿namespace TemplateMethod
+{
+    public sealed class CoffeeMaker : BeverageMaker
+    {
+        protected override void Brew()
+        {
+        }
+
+        protected override void AddCondiments()
+        {
+        }
+    }
+}
