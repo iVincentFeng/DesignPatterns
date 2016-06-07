4e703c0366025c8c828ec177724483768c26993d
Vincent Feng <iVincentFeng@live.com>
2016-06-07 18:58:32 +0800

    Prototype

---
+++ Prototype/IMyClonable.cs
@@ -0,0 +1,7 @@
+﻿namespace Prototype
+{
+    public interface IMyClonable
+    {
+        IMyClonable Clone();
+    }
+}
---
+++ Prototype/SomeClonable.cs
@@ -0,0 +1,22 @@
+﻿namespace Prototype
+{
+    public class SomeClonable : IMyClonable
+    {
+        public int IntProperty
+        {
+            get;
+            set;
+        }
+
+        public string StringProperty
+        {
+            get;
+            set;
+        }
+
+        public IMyClonable Clone()
+        {
+            return (IMyClonable)this.MemberwiseClone();
+        }
+    }
+}
