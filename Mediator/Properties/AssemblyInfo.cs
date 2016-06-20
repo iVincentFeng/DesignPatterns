fb5f8c795f859b61c1e8e7ef94306bea357ef1ce
Vincent Feng <iVincentFeng@live.com>
2016-06-20 23:22:17 +0800

    Assembly info updated.

--- Mediator/Properties/AssemblyInfo.cs
+++ Mediator/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DesignPatterns.Mediator")]
+[assembly: AssemblyTitle("DesignPatterns")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DesignPatterns.Mediator")]
+[assembly: AssemblyProduct("DesignPatterns")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
